217901543126a986514b47b425aa288b99bb0300
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-16 17:29:37 -0500

    add: prev and next buttons

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,7 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return { ...state, isLoading: true };
+
     case SET_STORIES:
       return {
         ...state,
@@ -22,14 +23,27 @@ const reducer = (state, action) => {
         ...state,
         hits: state.hits.filter(story => story.objectID !== action.payload)
       };
-    case HANDLE_SEARCH:
-      return {
-        ...state,
-        query: action.payload,
-        page: 0
-      };
+    case HANDLE_PAGE:
+      if (action.payload === 'inc') {
+        let nextPage = state.page + 1;
+        if (nextPage > state.nbPages) {
+          nextPage = 0;
+        }
+        return { ...state, page: nextPage };
+      }
+      if (action.payload === 'dec') {
+        let prevPage = state.page - 1;
+        if (prevPage < 0) {
+          prevPage = state.nbPages - 1;
+        }
+        return { ...state, page: prevPage };
+      }
+      break;
+    case HANDLE_SEARCH: {
+      return { ...state, query: action.payload, page: 0 };
+    }
     default:
-      throw new Error(`no matching ${action.type} action type`);
+      throw new Error(`no matching "${action.type}" action type`);
   }
 };
 export default reducer;
